dd6347e3cc4d8fb29cfca1ef2fe1d8ae52435fe9
Sam Starling <sam@soundcloud.com>
2015-11-06 15:05:21 +0100

    Remove println statements from MyOption

--- src/main/scala/com/samstarling/scalaplayground/MyOption.scala
+++ src/main/scala/com/samstarling/scalaplayground/MyOption.scala
@@ -2,18 +2,10 @@ package com.samstarling.scalaplayground
 
 case class MyOption[A](inner: A) {
   def map[B](f: A => B): MyOption[B] = {
-    val result = MyOption(f(inner))
-    println(s".map: ${inspect(inner)} => ${inspect(result)}")
-    result
+    MyOption(f(inner))
   }
 
   def flatMap[B](f: A => MyOption[B]): MyOption[B] = {
-    val result = f(inner)
-    println(s".flatMap: ${inspect(inner)} => ${inspect(result)}")
-    result
-  }
-
-  private def inspect(obj: Any): String = {
-    s"${obj.toString} (${obj.getClass})"
+    f(inner)
   }
 }
